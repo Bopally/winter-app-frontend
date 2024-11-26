a0a40c8e85d12b29a4eeb3e7edbe38cf3a00d488
Bopally <estelleb0707@gmail.com>
2024-11-26 12:08:43 +0100

    Create Style for the Home Page and Activities page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import "../styles/HomePage.css";
+
+function Footer() {
+  return (
+    <footer>
+      <p>
+        <a
+          href="https://github.com/Bopally"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
