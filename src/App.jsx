de455a36935443f8bf440401b7d6a65a1f8f4288
Bopally <estelleb0707@gmail.com>
2024-11-26 18:07:37 +0100

    Update route to create an account and review code for Instructor Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import CreateAccount from "./pages/CreateAccount";
 import Account from "./pages/AccountPage";
-
 import About from "./pages/AboutPage";
 import Accommodation from "./pages/Accommodation";
 import Activities from "./pages/Activities";
 import Gastronomy from "./pages/Gastronomy";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/account" element={<Account />} />
-
-        <Route path="/about" element={<About />} />
-        <Route path="/accommodation" element={<Accommodation />} />
-        <Route path="/activities" element={<Activities />} />
-        <Route path="/gastronomy" element={<Gastronomy />} />
-      </Routes>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/create-account" element={<CreateAccount />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/accommodation" element={<Accommodation />} />
+          <Route path="/activities" element={<Activities />} />
+          <Route path="/gastronomy" element={<Gastronomy />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
