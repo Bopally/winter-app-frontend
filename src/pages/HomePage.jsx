7abca69e2a6acf4cceb2588f46cdb1c2cef7f862
Bopally <estelleb0707@gmail.com>
2024-11-26 17:56:44 +0100

    Implement dynamic login dropdown and enhance account creation workflow

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,39 +1,62 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import "../styles/HomePage.css";
 
 function HomePage() {
   const [token, setToken] = useState(localStorage.getItem("authToken") || null);
   const navigate = useNavigate();
 
+  const handleLogout = () => {
+    setToken(null);
+    localStorage.removeItem("authToken");
+    navigate("/");
+  };
+
   const handleProfileClick = () => {
-    if (token) {
-      navigate("/account");
-    } else {
-      navigate("/login");
-    }
+    navigate("/account");
   };
 
   return (
     <div>
-      <h1>Serenity Mountain</h1>
       <nav>
-        <button onClick={handleProfileClick}>
-          {token ? "My Account" : "Login"}
-        </button>
         <p>
           <Link to="/about">ABOUT</Link>
         </p>
+        <div className="dropdown">
+          <button className="dropbtn">
+            {token ? "My Account" : "Connexion"}
+          </button>
+          <div className="dropdown-content">
+            {!token ? (
+              <>
+                <Link to="/login">Sign Up</Link>
+                <Link to="/create-account">Create an Account</Link>
+              </>
+            ) : (
+              <>
+                <button onClick={handleProfileClick}>My Account</button>
+                <button onClick={handleLogout}>Logout</button>
+              </>
+            )}
+          </div>
+        </div>
       </nav>
+      <header>
+        <h1>Serenity Mountain</h1>
+      </header>
+
       <main>
-        <p>
-          <Link to="/accommodation">ACCOMMODATION</Link>
-        </p>
-        <p>
-          <Link to="/activities">ACTIVITIES</Link>
-        </p>
-        <p>
+        <div className="row">
+          <div className="column main-link accommodation">
+            <Link to="/accommodation">ACCOMMODATION</Link>
+          </div>
+          <div className="column main-link activities">
+            <Link to="/activities">ACTIVITIES</Link>
+          </div>
+        </div>
+        <div className="full-width main-link gastronomy">
           <Link to="/gastronomy">GASTRONOMY</Link>
-        </p>
+        </div>
       </main>
     </div>
   );
