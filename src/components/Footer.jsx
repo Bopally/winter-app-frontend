11376ca2ac9f6d10119dfe90a9e548c57cd06a9b
Bopally <estelleb0707@gmail.com>
2024-11-28 17:19:15 +0100

    Enhance footer design, add GitHub & LinkedIn icons & redirect Privacy Policy to Not Found page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,40 @@
 import { Link } from "react-router-dom";
+
+import githubIcon from "../styles/images/github-icon.png";
+import linkedinIcon from "../styles/images/linkedin-icon.png";
+
 import "../styles/HomePage.css";
+import "../styles/Footer.css";
 
 function Footer() {
   return (
-    <footer>
-      <p>
-        <Link to="/about">ABOUT</Link>
-      </p>
-      <p>
-        <a
-          href="https://github.com/Bopally"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub
-        </a>
-      </p>
+    <footer className="footer">
+      <div className="footer-left">
+        <p className="footer-link">
+          <Link to="/about">Learn more about us!</Link>
+        </p>
+      </div>
+      <div className="footer-right">
+        <div className="footer-icons">
+          <a
+            href="https://github.com/Bopally"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={githubIcon} alt="GitHub" className="footer-icon" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/ballot-estelle/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={linkedinIcon} alt="LinkedIn" className="footer-icon" />
+          </a>
+        </div>
+        <p className="footer-link privacy-link">
+          <Link to="/not-found">Privacy Policy</Link>
+        </p>
+      </div>
     </footer>
   );
 }
